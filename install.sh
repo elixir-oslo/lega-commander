6b5ac3958577736e7dfb482ef82cc7ccdda1454e
Joshua Baskaran <joshbaskaran@gmail.com>
2023-10-17 11:25:48 +0200

    replaced amd64 with x86_64

--- install.sh
+++ install.sh
@@ -174,7 +174,7 @@ uname_os() {
 uname_arch() {
   arch=$(uname -m)
   case $arch in
-    x86_64) arch="amd64" ;;
+    x86_64) arch="x86_64" ;;
     x86) arch="386" ;;
     i686) arch="386" ;;
     i386) arch="386" ;;
@@ -207,6 +207,7 @@ uname_arch_check() {
   arch=$(uname_arch)
   case "$arch" in
     386) return 0 ;;
+    x86_64) return 0 ;;
     amd64) return 0 ;;
     arm64) return 0 ;;
     armv5) return 0 ;;
